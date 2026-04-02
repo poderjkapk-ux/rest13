27f859999cd77efbeca439f97f76b7cb1bbdeef5
Denis <poderjkapk@gmail.com>
2026-04-02 10:29:01 +0300

    3.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 25
-        versionName = "1.7.8"
+        versionName = "1.7.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
