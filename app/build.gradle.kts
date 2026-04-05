de6be806d49dbf43e14239720610c9412c954b05
Denis <poderjkapk@gmail.com>
2026-04-05 20:34:00 +0300

    3.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.restify.rest"
         minSdk = 26
         targetSdk = 35
-        versionCode = 29
-        versionName = "1.8.2"
+        versionCode = 30
+        versionName = "1.8.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
